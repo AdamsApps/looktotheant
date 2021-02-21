38516a8840973301eff4be3457239aa6c77cf67c
Adam Diament <adam@clevertree.io>
2021-02-21 12:26:03 +0200

    Add facebook app id

--- components/Head/index.js
+++ components/Head/index.js
@@ -11,7 +11,8 @@ const Head = ({ data }) => {
         <>
           {data.meta_title && <title>{data.meta_title}</title>}
           {data.meta_description && <meta name="description" content={data.meta_description} />}
-          <meta property="og:image" content="https://images.prismic.io/personal-training-jerusalem/9ce461e0-ee16-42cf-8de7-244384911695_Bethany066.jpg?auto=compress,format" />
+          <meta property="fb:app_id" content="850055168873710"/>
+          <meta property="og:image" content="https://images.prismic.io/personal-training-jerusalem/9ce461e0-ee16-42cf-8de7-244384911695_Bethany066.jpg?auto=compress,format&fbrefresh" />
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://www.personaltrainingjerusalem.com" />
           <meta property="og:title" content="Your fitness, your way"/ >
